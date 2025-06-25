c6a14f1eb6ac4fbf4965ee687626277404a4351a
Datss27 <93925305+Datss27@users.noreply.github.com>
2025-06-25 20:38:47 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 from pydantic_settings import BaseSettings
+
 class Settings(BaseSettings):
-    timezone: str = "Asia/Makassar"
     api_key: str
     telegram_token: str
     webhook_url: str
+    timezone: str = "Asia/Makassar"
+    base_url: str = "https://v3.football.api-sports.io"
     port: int = 8080
 
     class Config:
