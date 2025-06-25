078baa39c690f2d0a5f774337287d5bcc71a2833
Datss27 <93925305+Datss27@users.noreply.github.com>
2025-06-25 23:25:31 +0800

    Update excel_utils.py

--- excel_utils.py
+++ excel_utils.py
@@ -34,7 +34,7 @@ def create_workbook(fixtures):
         "Perbandingan", None
     ]
 
-    subheaders = ["" for _ in range(12)] + ["Home", "Away"] * 4
+    subheaders = ["" for _ in range(11)] + ["Home", "Away"] * 4
 
     ws.append(headers)
     ws.append(subheaders)
